2d482b6e6a7fa2cbf7bc21df17e0021a06f1cc46
Evgeniy <evgeniybogdashev@gmail.com>
2022-01-21 23:08:39 +0500

    migrations

--- db/migrate/20220120180440_add_all_table_default_value.rb
+++
@@ -1,7 +0,0 @@
-class AddAllTableDefaultValue < ActiveRecord::Migration[7.0]
-  def change
-    add_column(:answers, :correct, :boolean)
-    change_column_default(:tests, :level, 0)
-    change_column_default(:answers, :correct, false)
-  end
-end
