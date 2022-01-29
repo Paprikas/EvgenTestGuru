6f871d2cd08cbcdd902406cfe328cac7581d52dd
Evgeniy <evgeniybogdashev@gmail.com>
2022-01-29 16:05:08 +0500

    AddAssocAuthor

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,6 +3,7 @@ class Test < ApplicationRecord
   has_many :questions
   has_many :test_users
   has_many :users, through: :test_users
+  belongs_to :author, class_name: 'User'
 
   def self.tests_by_category(name_category)
     joins(:category).where('category.title = ?', name_category).order(title: :desc).pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :test_users
   has_many :tests, through: :test_users
+  has_many :created_tests, class_name: 'Test', foreign_key: :author_id
 
   def user_level_tests(level)
     tests.where(level: level)
