94a6e9d16271646742bcef76048497e3959aa831
Evgeniy <evgeniybogdashev@gmail.com>
2022-02-03 22:11:05 +0500

    AddScope

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,7 +5,9 @@ class Test < ApplicationRecord
   has_many :users, through: :test_users
   belongs_to :author, class_name: 'User'
 
-  def self.tests_by_category(name_category)
-    joins(:category).where('category.title = ?', name_category).order(title: :desc).pluck(:title)
-  end
+  scope :easy, -> {where(level: 0..1)}
+  scope :low, -> {where(level: 2..4)}
+  scope :hard, -> {where(level:5..Float::INFINITY)}
+  scope :tests_by_category, -> (name_category) {joins(:category).where('category.title = ?', name_category).order(title: :desc).pluck(:title)}
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,6 @@ class User < ApplicationRecord
   has_many :tests, through: :test_users
   has_many :created_tests, class_name: 'Test', foreign_key: :author_id
 
-  def user_level_tests(level)
-    tests.where(level: level)
-  end
+  scope :user_level_tests, -> (level)  {tests.where(level: level)}
 
 end
