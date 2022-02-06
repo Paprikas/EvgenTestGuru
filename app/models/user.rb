56d4008cc3be477d473d128b7b369d831cfdf641
Evgeniy <evgeniybogdashev@gmail.com>
2022-02-06 13:45:08 +0500

    ChangeScopeUserLevelTsts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :tests, through: :test_users
   has_many :created_tests, class_name: 'Test', foreign_key: :author_id
 
-  scope :user_level_tests, -> (level)  {joins(:tests).where('tests.level = ?', level)}
+  scope :user_level_tests, -> (level, user_id)  {joins(:tests).where('tests.level = ? and test_users.user_id = ?', level, user_id )}
 
   validates :name, :email, presence: true
 
