77b3cf72de214ec5aaa933d38d60506f4e292082
Evgeniy <evgeniybogdashev@gmail.com>
2022-02-06 22:58:50 +0500

    CorrectValidationAnswer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,9 +3,9 @@ class Answer < ApplicationRecord
 
   scope :correct, -> {where(correct: true)}
   validates :body, presence: true
-  validate :validate_answers_count, on: :create
+  validate :validate_answers_count?, on: :create
 
-  def validate_answers_count
+  def validate_answers_count?
     errors.add(:answer, "only 4 answers maximum") if question.answers.count > 4
   end
 end
