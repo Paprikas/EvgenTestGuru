c3b43298472f007fe5bbe641d02dd341d78da632
Evgen <evgeniybogdashev@gmail.com>
2022-02-16 21:32:58 +0500

    DB

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,24 @@
+class QuestionsController < ApplicationController   
+
+  def index
+
+  end
+
+  def show
+    @question = Question.find(params[id])
+  end  
+
+  def new
+  end  
+
+
+
+
+
+
+
+
+
+
+end
+
