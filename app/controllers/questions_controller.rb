13c6bb4b3d45f452b2b2008710cb985b265d5e44
Evgen <evgeniybogdashev@gmail.com>
2022-02-17 22:14:37 +0500

    QuestionsController

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,24 +1,49 @@
-class QuestionsController < ApplicationController   
+class QuestionsController < ApplicationController 
+  
+  before_action :find_test, only: %i[index new create]
+  before_action :find_question, only: %i[show destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
-
   end
 
   def show
-    @question = Question.find(params[id])
   end  
 
   def new
+  end 
+  
+  def create
+    question = @test.questions.create(question_params)
+    redirect_to test_questions_path
+    
+  end 
+  
+  def destroy
+    @question.destroy
+    redirect_to question_path
   end  
 
 
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
 
 
+  def find_question
+    @question = Question.find(params[:id])    
+  end  
 
+  def question_params
+    params.require(:question).permit(:body)
+  end  
 
-
-
-
+    def rescue_with_question_not_found
+        render plain: 'Вопрос не найден'
+    end
 
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "questions#index"
-
+  
+  
   resources :tests do
     resources :questions, shallow: true
   end 
