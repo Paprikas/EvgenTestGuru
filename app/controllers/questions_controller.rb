2ac8804a54fe3a382d18be33a893c839d6074c6b
Evgen <evgeniybogdashev@gmail.com>
2022-02-25 00:01:12 +0500

    CreateAllViews

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,33 +1,43 @@
 class QuestionsController < ApplicationController 
   
-  before_action :find_test, only: %i[index new create]
-  before_action :find_question, only: %i[show destroy]
+  before_action :find_test, only: %i[new create]
+  before_action :find_question, only: %i[show edit update destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
-
-  def index
-  end
 
   def show
   end  
 
   def new
+    @question = @test.questions.new
   end 
+
+  def edit
+  end
   
   def create
-    question = @test.questions.new(question_params)
-    if question.save
-      Render good
+     @question = @test.questions.new(question_params)
+    if @question.save
+      redirect_to @question
     else
-      Render bad
+      render plain: "bad"
     end
-    redirect_to test_questions_path
-    
+        
   end 
   
   def destroy
     @question.destroy
-    redirect_to question_path
+    
+    redirect_to test_path(@question.test)
+       
+  end  
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
+    end    
   end  
 
 
