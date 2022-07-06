e218d7e46a85a408df72cfad16199401a550218d
EvgeniyBogd <evgeniybogdashev@gmail.com>
2022-07-07 00:06:11 +0500

    CorrectGems

--- Gemfile
+++ Gemfile
@@ -60,6 +60,10 @@ gem 'jquery-rails'
 gem 'bootstrap', '~> 5.1.3'
 
 gem 'octicons_helper'
+
+gem 'net-http'
+
+gem 'faraday-retry'
  
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,8 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .css
+//= link_directory ../stylesheets .scss
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= link application.css
